1650c19294fe8baed9219c3cdf1ebab99896b20a
Cole Movsessian <movsessian@wisc.edu>
2023-12-06 23:43:07 +0000

    Web server?

--- WebServer.java
+++ WebServer.java
@@ -3,8 +3,10 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpContext;
 import java.io.OutputStream;
+import java.net.URI;
+import java.io.*;
 
-public class HelloWebServer {
+public class WebServer {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000),0);
         HttpContext context = server.createContext("/");
@@ -13,21 +15,16 @@ public class HelloWebServer {
 			System.out.println("Server Received HTTP Request");
 			
 			String requestedFile = path.toString().replace("/","");
-			if(requestedFile.startsWith("index")){
-				Headers responseHeaders = exchange.getResponseHeaders();
-			}else{
-				File inFile = new File(requestedFile);
-			}
-			
-			String line = exchange.getRequestURI().getQuery();
+			File inFile = new File(requestedFile);
 
-			String response;
+			//Headers responseHeaders = exchange.getResponseHeaders();
+			exchange.sendResponseHeaders(200, inFile.length());
 		
-			exchange.sendResponseHeaders(200, response.length());
-			exchange.getResponseHeaders().add("Content-type","text/html");
+			//exchange.sendResponseHeaders(200, response.length());
+			//exchange.getResponseHeaders().add("Content-type","text/html");
 
 			OutputStream outStream  = exchange.getResponseBody();
-			outStream.write(response.getBytes());
+			//outStream.write(response.getBytes());
 		
 			outStream.close();
 	    });
